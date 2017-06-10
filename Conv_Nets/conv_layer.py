fecac02e72c77ea4413a4171732a541a4c23eae8
Gavin <gavin_summers@hotmail.com>
2017-06-09 22:25:01 -0300

    Added solutions to conv_layer script

--- Conv_Nets/conv_layer.py
+++ Conv_Nets/conv_layer.py
@@ -21,17 +21,26 @@ def conv2d(input):
     # The shape of the filter bias is (output_depth,)
     # TODO: Define the filter weights `F_W` and filter bias `F_b`.
     # NOTE: Remember to wrap them in `tf.Variable`, they are trainable parameters after all.
-    F_W = ?
-    F_b = ?
+
+    #input_height = input.shape[1]
+    #input_weight = input.shape[2]
+
+    #new_height = (input_height - ? + 2 *P)/S+1
+
+    #print(input.shape[0])
+
+    F_W = tf.Variable(tf.random_normal([3,3,1,3]))
+    F_b = tf.Variable(tf.random_normal([3]))
     # TODO: Set the stride for each dimension (batch_size, height, width, depth)
-    strides = [?, ?, ?, ?]
+    strides = [1,1,1,1]
     # TODO: set the padding, either 'VALID' or 'SAME'.
-    padding = ?
+    padding = 'VALID'
     # https://www.tensorflow.org/versions/r0.11/api_docs/python/nn.html#conv2d
     # `tf.nn.conv2d` does not include the bias computation so we have to add it ourselves after.
     return tf.nn.conv2d(input, F_W, strides, padding) + F_b
 
 out = conv2d(X)
+print(out)
 
 
 
