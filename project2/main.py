2800a20d4af350e33656a28157a6e3468a0d821f
Gavin <gavin_summers@hotmail.com>
2017-06-13 16:27:52 -0300

    Added project 2

---
+++ project2/main.py
@@ -0,0 +1,10 @@
+# Probably dont need this, is for getting cifar data
+# from urllib.request import urlretrieve
+from os.path import isfile, isdir
+# Status bar for downloads maybe not need
+from tqdm import tqdm
+# Unittests python file in image_classification dir
+import problem_unittests as tests
+import tarfile
+
+cifar_path = 
